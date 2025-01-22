5324f1a9182ff7ab7b3f2035a51f1bc4ca4e90b8
Miliyonayalew <miliayalew@gmail.com>
2025-01-22 15:16:57 +0300

    refactor AboutPage layout for improved responsiveness and structure

--- src/app/(root)/about/page.tsx
+++ src/app/(root)/about/page.tsx
@@ -1,14 +1,13 @@
 export default function AboutPage() {
   return (
-    <main className="min-h-screen mx-auto w-full max-w-screen-xl">
-        <div className="container mx-auto px-4 py-16">
-        <h1 className="text-4xl font-bold mb-6">About Us</h1>
+    <main className="min-h-screen w-full pt-20  lg:min-h-screen mx-auto max-w-screen-2xl">        
+      <section className="w-full max-w-[95%] md:max-w-[90%] mx-auto px-4 py-8">        <h1 className="text-4xl font-bold mb-6">About Us</h1>
         <div className="prose max-w-none">
           <p className="text-lg text-gray-700">
             We are dedicated to providing exceptional repair services with a focus on quality and customer satisfaction.
           </p>
         </div>
-      </div>
+      </section>
     </main>
   );
 } 
