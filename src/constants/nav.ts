f2b852d6b6835eaa88e6416350df74d4fa70bc0a
Miliyonayalew <miliayalew@gmail.com>
2025-01-22 15:17:20 +0300

    add navbar constants for improved navigation management

---
+++ src/constants/nav.ts
@@ -0,0 +1,6 @@
+export const navbar = [
+  { href: "/", label: "Home" },
+  { href: "/about", label: "About" },
+  { href: "/contact", label: "Contact us" },
+  { href: "/faqs", label: "FAQs" },
+];
