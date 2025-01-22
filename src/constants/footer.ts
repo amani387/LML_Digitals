df7a1bf0aee6bdbce9ad2d7009c04343c3d2c1f5
Miliyonayalew <miliayalew@gmail.com>
2025-01-22 14:35:11 +0300

    add popular brands and mobiles constants for footer component

---
+++ src/constants/footer.ts
@@ -0,0 +1,22 @@
+export const popularBrands = [
+  "Apple",
+  "Samsung",
+  "Xiaomi",
+  "Realme",
+  "Oppo",
+  "Infinix",
+  "Infinix",
+  "OnePlus",
+  "Google",
+];
+
+export const popularMobiles = [
+  "iPhone 15 Pro",
+  "Samsung Galaxy S22",
+  "OnePlus 9R",
+  "Realme C57",
+  "Oppo A78",
+  "Xiaomi Redmi Note 13",
+  "Samsung Galaxy S23",
+  "Infinix Hot 40 Pro",
+];
