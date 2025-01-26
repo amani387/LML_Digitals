28d90f61e1d3a68484792503eb7ea7f9b31b1c23
Miliyonayalew <miliayalew@gmail.com>
2025-01-26 09:57:21 +0300

    refactor AboutPage layout and enhance value cards for improved design and user experience

--- src/app/(root)/about/page.tsx
+++ src/app/(root)/about/page.tsx
@@ -5,34 +5,56 @@ import { values } from "@/constants/about";
 
 const AboutPage = () => {
   return (
-    <main className="min-h-screen w-full pt-20 lg:min-h-screen mx-auto max-w-screen-2xl">
+    <main className="min-h-screen w-full pt-28 lg:min-h-screen mx-auto max-w-[1440px]">
       <section className="w-full max-w-[95%] md:max-w-[90%] mx-auto px-4 py-8">
-        <h1 className="text-4xl font-bold mb-6">About Us</h1>
-        <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-4 sm:gap-6 p-4 sm:p-6 mt-10 sm:mt-20 lg:mt-32">
-          {/* Title Card */}
-          <Card className="bg-white border-none col-span-1 sm:col-span-2" style={{ backgroundImage: "url('/undraw_testimonials_4c7y.svg')", backgroundSize: "contain", backgroundRepeat: "no-repeat", backgroundPosition: "center", alignSelf: "initial" }}>
-            <CardHeader>
-              <CardTitle className="text-right font-bold text-2xl sm:text-3xl lg:text-[40px] tracking-wide">
-                <strong>OUR VALUES</strong>
-              </CardTitle>
-            </CardHeader>
-          </Card>
+      <div className="grid grid-cols-1 md:grid-cols-2 gap-8">
+        <Card className="shadow-none border-none col-span-1 md:col-span-1 h-20 hover:cursor-pointer">
+        <CardHeader className="pb-2 px-2">
+          <CardTitle className="text-1xl lg:text-[20px] tracking-wide">
+          <strong>OUR VALUES</strong>
+          </CardTitle>
+          <CardDescription>
+          What we love and stand for every day
+          </CardDescription>
+        </CardHeader>
+        </Card>
 
-          {/* Individual Value Cards */}
-          {values.map((value, index) => (
-            <Card key={index} className="bg-white shadow-md group hover:shadow-lg hover:border-4 hover:border-green-100 transition-all duration-300 relative overflow-hidden">
-              <CardHeader>
-                <FontAwesomeIcon icon={value.icon} className={`w-6 h-6 text-blue-500 mb-2`} />
-                <CardTitle className="text-lg font-semibold">{value.title}</CardTitle>
-              </CardHeader>
-              <CardContent>
-                <CardDescription className="text-sm sm:text-base">
-                  {value.description}
-                </CardDescription>
-              </CardContent>
-            </Card>
-          ))}
-        </div>
+        {/* Individual Value Cards */}
+        {values.map((value, index) => (
+        <Card
+          key={index}
+          style={{
+            background: `linear-gradient(135deg, ${
+              index % 4 === 0
+          ? "#F9FAFB"
+          : index % 4 === 1
+          ? "#E7FAEF"
+          : index % 4 === 2
+          ? "#F2FFF9"
+          : "#ffffff"
+            }, #CEDCF8)`,
+          }}
+          className={`shadow-sm border-none group hover:cursor-pointer transition-all duration-300 relative overflow-hidden ${
+            index % 2 === 0 ? "row-span-2" : "row-span-2"
+          } hover:shadow-lg hover:scale-105 hover:bg-opacity-90`}
+        >
+          <CardHeader>
+            <FontAwesomeIcon
+              icon={value.icon}
+              className={`w-6 h-6 text-blue-500 mb-2`}
+            />
+            <CardTitle className="text-lg font-semibold">
+              {value.title}
+            </CardTitle>
+          </CardHeader>
+          <CardContent>
+            <CardDescription className="text-sm sm:text-base">
+              {value.description}
+            </CardDescription>
+          </CardContent>
+        </Card>
+        ))}
+      </div>
       </section>
     </main>
   );
