16677a47a9187bba8731505b4364e3cfb523111d
Miliyonayalew <miliayalew@gmail.com>
2025-02-10 00:36:51 +0300

    feat: add UI components for labels, toasts, popovers, dialogs, and testimonials

---
+++ src/constants/testimonials.ts
@@ -0,0 +1,44 @@
+export const testimonials = [
+  {
+    author: "John Doe",
+    title: "CEO, Company",
+    message: `The Cursor tab completion while coding is occasionally so magic it defies reality - about ~25% of the time it is anticipating exactly what I want to do. <br/> It is enough to make you believe that eventually you'll be able to code at the speed of thought.`,
+    image: "/images/testimonial.webp",
+  },
+  {
+    author: "Jane Doe",
+    title: "CTO, Company",
+    message: `Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nulla auctor, justo et
+        fringilla aliquam, mi nunc aliquet nunc, ac varius lacus mi nec odio. Cras vitae 
+        purus ac nunc ultricies aliquam. Nulla facilisi. Sed ac nunc sit amet nunc tincidunt aliquam. 
+        Proin nec ex nec elit ultricies ultricies. Nulla facilisi. Sed ac nunc sit amet nunc tincidunt aliquam.`,
+    image: "/images/testimonial.webp",
+  },
+  {
+    author: "John Doe",
+    title: "CEO, Company",
+    message: `Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nulla auctor, justo et
+        fringilla aliquam, mi nunc aliquet nunc, ac varius lacus mi nec odio. Cras vitae 
+        purus ac nunc ultricies aliquam. Nulla facilisi. Sed ac nunc sit amet nunc tincidunt aliquam. 
+        Proin nec ex nec elit ultricies ultricies. Nulla facilisi. Sed ac nunc sit amet nunc tincidunt aliquam.`,
+    image: "/images/testimonial.webp",
+  },
+  {
+    author: "Jane Doe",
+    title: "CTO, Company",
+    message: `Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nulla auctor, justo et
+        fringilla aliquam, mi nunc aliquet nunc, ac varius lacus mi nec odio. Cras vitae 
+        purus ac nunc ultricies aliquam. Nulla facilisi. Sed ac nunc sit amet nunc tincidunt aliquam. 
+        Proin nec ex nec elit ultricies ultricies. Nulla facilisi. Sed ac nunc sit amet nunc tincidunt aliquam.`,
+    image: "/images/testimonial.webp",
+  },
+  {
+    author: "John Doe",
+    title: "CEO, Company",
+    message: `Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nulla auctor, justo et
+        fringilla aliquam, mi nunc aliquet nunc, ac varius lacus mi nec odio. Cras vitae 
+        purus ac nunc ultricies aliquam. Nulla facilisi. Sed ac nunc sit amet nunc tincidunt aliquam. 
+        Proin nec ex nec elit ultricies ultricies. Nulla facilisi. Sed ac nunc sit amet nunc tincidunt aliquam.`,
+    image: "/images/testimonial.webp",
+  },
+];
